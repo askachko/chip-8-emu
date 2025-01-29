971c67be39b729a7c20f6fca94a8db916f19fe90
askachko <alexandermskachko@gmail.com>
2025-01-28 19:54:30 -0500

    feat: Machine state machine and cleanup functions

--- main.c
+++ main.c
@@ -20,6 +20,20 @@ typedef struct {
   uint32_t scale_factor;
 } config_t;
 
+typedef enum {
+  QUIT,
+  RUNNING,
+  PAUSED
+} emulator_state_t;
+
+typedef struct {
+  emulator_state_t state;
+} chip8_t;
+
+bool init_chip8(chip8_t *chip8) {
+    chip8->state = RUNNING;
+    return true;
+};
 
 bool init_sdl(sdl_t *sdl, const config_t config) {
   if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_TIMER) != 0) {
@@ -47,6 +61,12 @@ bool init_sdl(sdl_t *sdl, const config_t config) {
   return true;
 }
 
+void final_cleanup(const sdl_t *sdl) {
+  SDL_DestroyRenderer(sdl->renderer);
+  SDL_DestroyWindow(sdl->window);
+  SDL_Quit();
+}
+
 bool set_config_from_cli_args(config_t *config, const int argc, char **argv) {
 
   // set default:
@@ -63,9 +83,45 @@ bool set_config_from_cli_args(config_t *config, const int argc, char **argv) {
   return true;
 }
 
-int main(int argc, char **argv){
-  (void)argc;
-  (void)argv;
+void update_screen(const sdl_t *sdl) {
+    SDL_RenderPresent(sdl->renderer);
+}
+
+void clear_screen(const config_t *config, const sdl_t *sdl) {
+  const uint8_t r = (config->bg_color >> 24) & 0xFF;
+  const uint8_t g = (config->bg_color >> 16) & 0xFF;
+  const uint8_t b = (config->bg_color >> 8) & 0xFF;
+  const uint8_t a = config->bg_color & 0xFF;
+
+  SDL_SetRenderDrawColor(sdl->renderer, r, g, b, a); // red
+  SDL_RenderClear(sdl->renderer);
+}
+
+void handle_input(chip8_t *chip8) {
+  SDL_Event event;
+
+  while (SDL_PollEvent(&event)) {
+    switch (event.type) {
+    case SDL_QUIT:
+      chip8->state = QUIT;
+      return;
+
+    case SDL_KEYDOWN:
+        switch (event.key.keysym.sym) {
+            case (SDLK_ESCAPE): chip8->state = QUIT ;
+
+        };
+        break;
+
+    case SDL_KEYUP:
+      break;
+
+    default:
+      break;
+    }
+  }
+}
+
 int main(int argc, char **argv) {
   config_t config = {0};
   if (!set_config_from_cli_args(&config, argc, argv))
@@ -76,11 +132,24 @@ int main(int argc, char **argv) {
   if (!init_sdl(&sdl, config))
     exit(EXIT_FAILURE);
 
-  printf("Hello world");
   chip8_t chip8 = {0};
   if (!init_chip8(&chip8))
     exit(EXIT_FAILURE);
 
+  clear_screen(&config, &sdl);
+
+  while (chip8.state != QUIT) {
+    // Get_time();
+    handle_input(&chip8);
+
+    clear_screen(&config, &sdl);
+    update_screen(&sdl);
+
+    // 60 fps dela
+    SDL_Delay(16); // 16 - actual time elapsed
+  }
+
+  final_cleanup(&sdl);
   printf("No errors");
   exit(EXIT_SUCCESS);
 
